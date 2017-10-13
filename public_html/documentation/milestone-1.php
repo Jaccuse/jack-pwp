2c4fa2e781cee5b0861212ca9a80769a98d57149
Jaccuse <guck@jack-arnold.com>
2017-10-13 10:20:25 -0600

    Added paragraphs and text

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -5,6 +5,17 @@
         <title>Jack's PWP</title>
     </head>
     <body>
-
+        <p>
+            <h2>Purpose</h2>
+            This site will be a front-end-focused professional profile.
+        </p>
+        <p>
+            <h2>Audience</h2>
+            This site will be designed with the web development community in mind. The target audience will include people such as recruiters and other web developers.
+        </p>
+        <p>
+            <h2>Goal</h2>
+            The goal of this website will be to put a professional image on myself and give potential employers and collaborators a channel by which to contact me.
+        </p>
     </body>
 </html>
