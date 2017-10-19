739e837a32f7c77ddf909fe6955bfcc96234adaf
Jaccuse <guck@jack-arnold.com>
2017-10-18 23:09:37 -0600

    Added persona paragraph

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -17,5 +17,18 @@
             <h2>Goal</h2>
             The goal of this website will be to put a professional image on myself and give potential employers and collaborators a channel by which to contact me.
         </p>
+        <p>
+            <h2>Persona</h2>
+            <ul>
+                <li><strong>Name: </strong>Alma</li>
+                <li><strong>Age: </strong>35</li>
+            </ul>
+            <ul>
+                <li><strong>Profession: </strong> Alma works as a hiring manager at a somewhat large web-focused tech company</li>
+                <li><strong>Technology: </strong> Alma uses a company-supplied current generation MacBook which she can be found using at any time in any place, she also chooses to use iPhones, which she always keeps up to date.</li>
+                <li><strong>Behavior: </strong> Alma will often stay up late catching up or getting ahead on work, she also travels to recruit for her company.</li>
+                <li><strong>Goals: </strong> Alma wants to hire people of high soft skills, hard skills, and intelligence for various positions at her company.</li>
+            </ul>
+        </p>
     </body>
 </html>
