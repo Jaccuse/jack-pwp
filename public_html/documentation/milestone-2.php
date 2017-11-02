8ea0807d31736c7c0065f7b7dd8ecbc7eff75b2f
Jaccuse <guck@jack-arnold.com>
2017-11-02 09:20:40 -0600

    Updated milestone-2.php with links

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -8,6 +8,8 @@
         <main>
             <h2>Content Strategy</h2>
             This website will be a showcase of my skills that I am learning. The main content will be in the middle of the page with a navbar on the side. My relevant social media links will be at the bottom.
+            <a href="https://bootcamp-coders.cnm.edu/~jarnold33/jack-pwp/images/pwp-wireframe%20(1).svg">Desktop Wireframe</a>
+            <a href="https://bootcamp-coders.cnm.edu/~jarnold33/jack-pwp/images/pwp-mobile-wireframe.svg">Mobile Wireframe</a>
         </main>
     </body>
 </html>
