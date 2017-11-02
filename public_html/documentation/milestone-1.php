5567545886fb2b82e526e1a5cb6dc39ed6773aa4
Jaccuse <guck@jack-arnold.com>
2017-11-02 09:10:23 -0600

    Updated milestone-1.php

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -7,31 +7,23 @@
     </head>
     <body>
         <main>
-            <p>
-                <h2>Purpose</h2>
-                This site will be a front-end-focused professional profile.
-            </p>
-            <p>
-                <h2>Audience</h2>
-                This site will be designed with the web development community in mind. The target audience will include people such as recruiters and other web developers.
-            </p>
-            <p>
-                <h2>Goal</h2>
-                The goal of this website will be to put a professional image on myself and give potential employers and collaborators a channel by which to contact me.
-            </p>
-                <h2>Persona</h2>
+            <h2>Purpose</h2>
+            This site will be a front-end-focused professional profile.
+            <h2>Audience</h2>
+            This site will be designed with the web development community in mind. The target audience will include people such as recruiters and other web developers.
+            <h2>Goal</h2>
+            The goal of this website will be to put a professional image on myself and give potential employers and collaborators a channel by which to contact me.
+            <h2>Persona</h2>
         </main>
-        <p>
-            <ul>
-                <li><strong>Name: </strong>Alma</li>
-                <li><strong>Age: </strong>35</li>
-            </ul>
-            <ul>
-                <li><strong>Profession: </strong> Alma works as a hiring manager at a somewhat large web-focused tech company</li>
-                <li><strong>Technology: </strong> Alma uses a company-supplied current generation MacBook which she can be found using at any time in any place, she also chooses to use iPhones, which she always keeps up to date.</li>
-                <li><strong>Behavior: </strong> Alma will often stay up late catching up or getting ahead on work, she also travels to recruit for her company.</li>
-                <li><strong>Goals: </strong> Alma wants to hire people of high soft skills, hard skills, and intelligence for various positions at her company.</li>
-            </ul>
-        </p>
+        <ul>
+            <li><strong>Name: </strong>Alma</li>
+            <li><strong>Age: </strong>35</li>
+        </ul>
+        <ul>
+            <li><strong>Profession: </strong> Alma works as a hiring manager at a somewhat large web-focused tech company</li>
+            <li><strong>Technology: </strong> Alma uses a company-supplied current generation MacBook which she can be found using at any time in any place, she also chooses to use iPhones, which she always keeps up to date.</li>
+            <li><strong>Behavior: </strong> Alma will often stay up late catching up or getting ahead on work, she also travels to recruit for her company.</li>
+            <li><strong>Goals: </strong> Alma wants to hire people of high soft skills, hard skills, and intelligence for various positions at her company.</li>
+        </ul>
     </body>
 </html>
