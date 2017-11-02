4ef364b53e17b72cf14b9ecc5a751159d848ed12
Jaccuse <guck@jack-arnold.com>
2017-11-02 09:08:12 -0600

    Committed milestone-2.php

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -3,22 +3,25 @@
     <head>
        <meta charset="utf-8"/>
         <title>Jack's PWP</title>
+        <link rel="stylesheet" type="text/css" href="style.css"/>
     </head>
     <body>
+        <main>
+            <p>
+                <h2>Purpose</h2>
+                This site will be a front-end-focused professional profile.
+            </p>
+            <p>
+                <h2>Audience</h2>
+                This site will be designed with the web development community in mind. The target audience will include people such as recruiters and other web developers.
+            </p>
+            <p>
+                <h2>Goal</h2>
+                The goal of this website will be to put a professional image on myself and give potential employers and collaborators a channel by which to contact me.
+            </p>
+                <h2>Persona</h2>
+        </main>
         <p>
-            <h2>Purpose</h2>
-            This site will be a front-end-focused professional profile.
-        </p>
-        <p>
-            <h2>Audience</h2>
-            This site will be designed with the web development community in mind. The target audience will include people such as recruiters and other web developers.
-        </p>
-        <p>
-            <h2>Goal</h2>
-            The goal of this website will be to put a professional image on myself and give potential employers and collaborators a channel by which to contact me.
-        </p>
-        <p>
-            <h2>Persona</h2>
             <ul>
                 <li><strong>Name: </strong>Alma</li>
                 <li><strong>Age: </strong>35</li>
---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <title>Jack's PWP</title>
+    </head>
+    <body>
+        <main>
+            <p>
+                <h2>Content Strategy</h2>
+                This website will be a showcase of my skills that I am learning. The main content will be in the middle of the page with a navbar on the side. My relevant social media links will be at the bottom.
+            </p>
+        </main>
+    </body>
+</html>
