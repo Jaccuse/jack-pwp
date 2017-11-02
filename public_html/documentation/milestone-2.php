6a82621f44f72279cd71b333d173429577e46653
Jaccuse <guck@jack-arnold.com>
2017-11-02 09:22:18 -0600

    Updated milestone-2.php with css

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8"/>
         <title>Jack's PWP</title>
+        <link rel="stylesheet" type="text/css" href="style.css"/>
     </head>
     <body>
         <main>
